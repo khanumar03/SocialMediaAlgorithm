5d0538a3768638bfcaf3b1d509667ca14ea597a5
khanumar03 <khanumar28122003@outlook.in>
2023-09-19 01:08:44 +0530

    commit

--- server.ts
+++ server.ts
@@ -1,16 +1,15 @@
 import express, { Application } from "express";
 import mongoose from "mongoose";
 import "./models/User";
+import { config } from "dotenv";
+config();
 
 const app: Application = express();
 
 async function _init() {
   app.use(express.json());
 
-  await mongoose
-    .connect
-    // "mongodb+srv://algorithm:khan123456@cluster0.cpovggi.mongodb.net/?retryWrites=true&w=majority"
-    ();
+  await mongoose.connect(process.env.DB_URI as string);
 
   app.get("/", (req, res) => {
     res.json({ message: "hello world" });
